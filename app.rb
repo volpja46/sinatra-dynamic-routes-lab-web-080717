daeefbd95835bb25035551f6b1a6df82e00537d6
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-08-28 18:24:28 -0400

    done

--- app.rb
+++ app.rb
@@ -1,6 +1,42 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+  class App < Sinatra::Base
   # Write your code here!
 
+      get '/reversename/:name' do
+       params[:name].reverse
+    end
+
+    get '/square/:number' do
+    (params[:number].to_i ** 2).to_s
+  end
+
+
+  # Create a dynamic route at get '/say/:number/:phrase' that accepts a number and a phrase and returns that phrase in a string the number of times given.
+get '/say/:number/:phrase' do
+  return_value = ""
+
+    params[:number].to_i.times do
+      return_value += params[:phrase]
+      end
+      return_value
+    end
+
+     get '/say/:word1/:word2/:word3/:word4/:word5' do
+       "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+     end
+
+
+      get '/:operation/:number1/:number2' do
+            case params[:operation]
+           when "add"
+             "#{params[:number1].to_i + params[:number2].to_i}"
+           when "subtract"
+             "#{params[:number1].to_i - params[:number2].to_i}"
+           when "multiply"
+             "#{params[:number1].to_i * params[:number2].to_i}"
+           when "divide"
+             "#{params[:number1].to_i / params[:number2].to_i}"
+           end
+         end
 end
